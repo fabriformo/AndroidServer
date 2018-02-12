526112ecc3b6a4574b676c41bc8be8dbfd832a4b
fabriformo <fabriformo@gmail.com>
2018-02-12 17:43:18 +0100

    FUNZIONA! Con 2 topic e gestisce l'iscrizione

--- src/main/scala/AndroidServer.scala
+++ src/main/scala/AndroidServer.scala
@@ -10,12 +10,15 @@ import scala.concurrent.Future
 
 object AndroidServer extends App {
   case class User(name: String, sock: Socket, in: ObjectInputStream, out: ObjectOutputStream)
+  {
+    var topic = 0
+  }
   val users = new ConcurrentHashMap[String, User]().asScala
   var count= 0
   var j = 0
   var obj:Object = _
-  var temperatura: String = _
-  var pressione: String = _
+  var temperatura: String = "0"
+  var pressione: String = "0"
   println("HI")
 
 
@@ -37,7 +40,7 @@ object AndroidServer extends App {
       Future{
         //out.println("What is your name")
         val name=  sock.getInetAddress.toString
-        println("Si e' connesso l'utente "+ name)
+        println("Si e' connesso l'utente "+ name+" :)")
         val user= User(name, sock, in, out)
         users += name-> user
         count= count +1
@@ -72,32 +75,89 @@ object AndroidServer extends App {
   def doChat(user: User): Unit = {
     nonblockingRead(user.in, user.sock).foreach { input =>
       if(input.tipo == 0){
+        val address = user.sock.getInetAddress.toString
         user.sock.close()
         users -= user.name
         println("GONE :(")
+        println("Si e' disconnesso l'utente "+ address+" :(")
         //obj = null
-      } else if(input.tipo == 2) {
+      }
+      else if(input.tipo == 1){
+        if(input.topic == 1){
+          if(user.topic == 0){
+            user.topic = 1
+            println("L'utente "+user.sock.getInetAddress.toString+" si e' iscritto al topic 1")
+            user.out.writeObject(new MyMessage(2,temperatura,1))
+          }
+          else if(user.topic == 2){
+            user.topic = 3
+            println("L'utente "+user.sock.getInetAddress.toString+" si e' iscritto a entrambi i topic")
+            user.out.writeObject(new MyMessage(2,temperatura,1))
+          }
+
+        }
+        else if(input.topic == 2){
+          if(user.topic == 0) {
+            user.topic = 2
+            println("L'utente "+user.sock.getInetAddress.toString+" si e' iscritto al topic 2")
+            user.out.writeObject(new MyMessage(2,pressione,2))
+          }
+          else if(user.topic == 1){
+            user.topic = 3
+            println("L'utente "+user.sock.getInetAddress.toString+" si e' iscritto a entrambi i topic")
+            user.out.writeObject(new MyMessage(2,pressione,2))
+          }
+        }
+      }
+      else if(input.tipo == 2) {
         //for((n, u) <- users){
         //u.out.println(user.name+" : "+input)
         //u.out.println(j)
         //}
         if(input.topic == 1){
           println(user.name+" ha aggiornato la temperatura a: "+input.message)
-          //temperatura = input.message
-          for((n, u) <- users)
-            u.out.writeObject(input)
+          temperatura = input.message
+          for((n, u) <- users){
+            if(u.topic == 1 || u.topic == 3)
+              u.out.writeObject(input)
+          }
         }
         else if(input.topic == 2){
           println(user.name+" ha aggiornato la pressione a: "+input.message)
-          //pressione = input.message
-          for((n, u) <- users)
-            u.out.writeObject(input)
+          pressione = input.message
+          for((n, u) <- users){
+            if(u.topic == 2 || u.topic == 3)
+              u.out.writeObject(input)
+          }
         }
         /*if(input == 1){
 
         }*/
         //j +=1
         //obj = null
+      }
+      else if(input.tipo == 3)
+      {
+        if(input.topic == 1){
+          if(user.topic == 1){
+            user.topic = 0
+            println("L'utente "+user.sock.getInetAddress.toString+" NON e' piu' iscritto al topic 1")
+          }
+          else if(user.topic == 3){
+            user.topic = 2
+            println("L'utente "+user.sock.getInetAddress.toString+" adesso e' iscritto SOLO al topic 1")
+          }
+        }
+        else if(input.topic == 2){
+          if(user.topic == 2){
+            user.topic = 0
+            println("L'utente "+user.sock.getInetAddress.toString+" NON e' piu' iscritto al topic 2")
+          }
+          else if(user.topic == 3){
+            user.topic = 1
+            println("L'utente "+user.sock.getInetAddress.toString+" adesso e' iscritto SOLO al topic 1")
+          }
+        }
       }
       obj = null
     }
--- src/main/scala/mymessage/MyMessage.scala
+++ src/main/scala/mymessage/MyMessage.scala
@@ -1,7 +1,7 @@
 package mymessage
 
 class MyMessage(_tipo:Int, _message:String, _topic:Int) extends Serializable {
-  def tipo: Int = _tipo //0 disconnessione 1 iscrizione 2 aggiornamento
+  def tipo: Int = _tipo //0 disconnessione 1 iscrizione 2 aggiornamento 3 disiscrizione
   def message: String = _message
-  def topic: Int = _topic
+  def topic: Int = _topic //0 nessuno 1 temperatura 2 pressione 3 entrambi
 }
